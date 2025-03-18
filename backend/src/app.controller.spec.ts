801b3a716ef44d25855d76d65ea2e8434c46773b
valtonp <paulvalton@icloud.com>
2025-03-18 17:06:15 +0100

    test: add test

---
+++ backend/src/app.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+    let appController: AppController;
+    let appService: AppService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+        }).compile();
+
+        appController = module.get<AppController>(AppController);
+        appService = module.get<AppService>(AppService);
+    });
+
+    describe('root', () => {
+        it('should return "Hello World!"', () => {
+            expect(appController.getHello()).toBe('Hello World!');
+        });
+    });
+});
